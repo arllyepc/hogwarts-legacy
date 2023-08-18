34856c5db0aa1f4863bd97065d74ae783c0d3c78
arllyepc <arllyepc@gmail.com>
2023-08-18 19:28:54 -0300

    atualização

--- js/scripts/main.js
+++ js/scripts/main.js
@@ -1,44 +1,60 @@
 const menuLang = document.querySelector(".lang");
 const menuLangList = document.querySelector(".lang-list");
 
+document.querySelectorAll(".lang-list li").forEach((li) => {
+  li.addEventListener("click", function () {
+    const language = this.textContent;
+    translateTo(language);
+  });
+});
+
+function translateTo(language) {
+  if (!translations[language]) return; // se não tiver tradução para o idioma selecionado, apenas retorne
+
+  document.querySelectorAll("[data-translate]").forEach((element) => {
+    const key = element.getAttribute("data-translate");
+    if (translations[language][key]) {
+      element.textContent = translations[language][key];
+    }
+  });
+
+  // Atualiza o valor do idioma na <div class="lang">
+  document.querySelector(".lang span").textContent = language;
+}
+
 menuLang.addEventListener("click", (e) => {
   menuLangList.classList.toggle("active");
 });
 
-const texts = {
-  header: {
-    logoAlt: {
-      pt: "Logo do Hogwarts Legacy",
-      en: "Hogwarts Legacy logo",
-    },
-    navLinks: [
-      { text: { pt: "Início", en: "Home" }, href: "#" },
-      { text: { pt: "Sobre", en: "About" }, href: "#" },
-      { text: { pt: "Contato", en: "Contact" }, href: "#" },
-    ],
-    button: {
-      pt: "Compre agora na Steam",
-      en: "Buy now on Steam",
-    },
+const translations = {
+  BRA: {
+    "Principal": "Principal",
+    "Sobre": "Sobre",
+    "Funcionalidades": "Funcionalidades",
+    "Requisitos de sistema": "Requisitos de sistema",
+    "VIVA EXPERIÊNCIAS PELO MUNDO DE HARRY POTTER":
+      "VIVA EXPERIÊNCIAS PELO MUNDO DE HARRY POTTER",
+    "Compre agora na Steam": "Compre agora na Steam",
+    Preco: "R$ 255,11",
   },
-  hero: {
-    title: {
-      pt: "Hogwarts Legacy",
-      en: "Hogwarts Legacy",
-    },
-    subtitle: {
-      pt: "VIVA EXPERIÊNCIAS PELO MUNDO DE HARRY POTTER",
-      en: "LIVE EXPERIENCES IN THE WORLD OF HARRY POTTER",
-    },
-    button: {
-      pt: "Compre agora na Steam",
-      en: "Buy now on Steam",
-    },
-    price: {
-      pt: "R$ 255,11",
-      en: "$59.99",
-    },
+  ENG: {
+    "Principal": "Home",
+    "Sobre": "About",
+    "Funcionalidades": "Features",
+    "Requisitos de sistema": "System Requirements",
+    "VIVA EXPERIÊNCIAS PELO MUNDO DE HARRY POTTER":
+      "LIVE EXPERIENCES THROUGH THE WORLD OF HARRY POTTER",
+    "Compre agora na Steam": "Buy now on Steam",
+    "Preco": "$ 69.99",
+  },
+  ESP: {
+    "Principal": "Inicio",
+    "Sobre": "Acerca",
+    "Funcionalidades": "Características",
+    "Requisitos de sistema": "Requisitos del sistema",
+    "VIVA EXPERIÊNCIAS PELO MUNDO DE HARRY POTTER":
+      "VIVE EXPERIENCIAS POR EL MUNDO DE HARRY POTTER",
+    "Compre agora na Steam": "Compra ahora en Steam",
+  "Preco": "€ 59.99",
   },
 };
-
-const language = "pt"; // Defina a opção de idioma aqui
